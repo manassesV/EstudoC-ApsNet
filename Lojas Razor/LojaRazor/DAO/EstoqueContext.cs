efcbc717fa50bbdefa0a17f1e2737d4c359af7a1
Manassés <manavitorino@gmail.com>
2018-02-03 09:38:25 -0200

    Aplicação Asp Net estudando Razor, melhorando a view de sua aplicação.

---
+++ Lojas Razor/LojaRazor/DAO/EstoqueContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using LojaRazor.Models;
+
+namespace LojaRazor.DAO
+{
+    public class EstoqueContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+
+        public EstoqueContext()
+            : base("ConexaoERP")
+        {
+            Database.SetInitializer<EstoqueContext>(null);
+        }
+
+   
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var __DBOwner = "dbo";
+
+            modelBuilder.Entity<Usuario>().ToTable("Usuario", schemaName: __DBOwner);
+            modelBuilder.Entity<Produto>().ToTable("Produtos", schemaName: __DBOwner);
+            modelBuilder.Entity<Departamento>().ToTable("Departamentos", schemaName: __DBOwner);
+            modelBuilder.Entity<Categoria>().ToTable("Categorias", schemaName: __DBOwner);
+        }
+
+        //protected override bool ShouldValidateEntity(DbEntityEntry entityEntry)
+        //{
+        //    //return base.ShouldValidateEntity(entityEntry)
+        //    //    || (entityEntry.State == EntityState.Deleted
+        //    //       && entityEntry.Entity is Con);
+        //}
+    }
+}
---
+++ Lojas Razor/dbo.Table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Usuario]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nome] VARCHAR(50) NULL, 
+    [Email] VARCHAR(50) NULL, 
+    [Senha] VARCHAR(50) NULL, 
+    [DataDeNascimento] DATETIME NULL, 
+    [Endereco] VARCHAR(50) NULL, 
+    [Complemento] VARCHAR(50) NULL, 
+    [CEP] VARCHAR(50) NULL, 
+    [Observacoes] VARCHAR(50) NULL, 
+    [RecebePromocoes] VARCHAR(50) NULL
+)
